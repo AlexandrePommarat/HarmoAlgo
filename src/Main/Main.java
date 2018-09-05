6048952359a91a0159e19cf51e783b020f1ca8df
DimGael <dimgael98@gmail.com>
2018-09-05 11:59:08 +0200

    ListeFiche finie, création du main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,14 +2,21 @@ package Main;
 
 import java.util.Scanner;
 
+import listefiche.FicheChaine;
+import listefiche.ListeFiche;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		boolean quitter = false;
-		
-		while(!quitter){
-			System.out.println("S�lectionnez une action � effectuer:");
+		final ListeFiche listeFiche = new ListeFiche();
+		listeFiche.ajouterFiche(new FicheChaine("Alexandre", "Pommarat"));
+		listeFiche.ajouterFiche(new FicheChaine("Gael", "Dim"));
+
+		while (!quitter) {
+			System.out.println("\n---------------------------------------");
+			System.out.println("Selectionnez une action a effectuer:");
 			System.out.println("A. Afficher fiches");
 			System.out.println("B. Afficher une fiche");
 			System.out.println("C. Ajouter une fiche");
@@ -17,15 +24,64 @@ public class Main {
 			System.out.println("E. Rechercher une fiche");
 			System.out.println("F. Trier le tableau");
 			System.out.println("G. Quitter");
-			
+			System.out.println("---------------------------------------\n");
+
 			Scanner sc = new Scanner(System.in);
-			String reponse;
-			reponse = sc.nextLine();
-			
-			
-			
+			char reponse;
+
+			System.out.println("\nRéponse : ");
+			// Récupération de la réponse en majuscule
+			reponse = Character.toUpperCase(sc.nextLine().charAt(0));
+
+			switch (reponse) {
+			case 'G':
+				quitter = true;
+				break;
+
+			case 'A':
+				afficherFiches(listeFiche);
+				break;
+
+			case 'B':
+				System.out.println("Afficher la fiche numéro : ");
+				final int index = sc.nextInt();
+				Fiche f = listeFiche.getFiche(index);
+				if (f == null) {
+					System.out.println("Ce contact n'existe pas !");
+				} else {
+					//Affichage de la fiche
+					afficherFiche(f);
+				}
+
+			default:
+				System.out.println("Reponse inconnue");
+			}
 		}
+
+		System.out.println("Au revoir !!");
+
+	}
+
+	private static void afficherFiche(Fiche f) {
 		
+	}
+
+	private static void afficherFiches(ListeFiche listeFiche) {
+		if (!listeFiche.isEmpty()) {
+			FicheChaine f = listeFiche.getFicheChaine(1);
+			int indexFiche = 1;
+			System.out.println("\n-----------------------");
+			do {
+				System.out.println(indexFiche + " : " + f.getPrenom() + " " + f.getNom());
+				f = f.getFicheSuivante();
+				indexFiche++;
+			} while (f != null);
+			System.out.println("-----------------------\n");
+
+		} else {
+			System.out.println("Vous n'avez pas de fiches d'adresses !");
+		}
+
 	}
 
 }
--- src/listefiche/ListeFiche.java
+++ src/listefiche/ListeFiche.java
@@ -1,127 +1,155 @@
 package listefiche;
 
+import Main.Fiche;
 
 /**
- * Classe permettant de creer une liste de {@link FicheChaine} et de les manipuler, fiches indexées en commençant par 1.
- * @author gael 
+ * Classe permettant de creer une liste de {@link FicheChaine} et de les
+ * manipuler, fiches indexées en commençant par 1.
+ * 
+ * @author gael
  *
  */
 public class ListeFiche {
-	
+
 	/**
 	 * La Premiere fiche
 	 */
 	private FicheChaine fiche;
-	
-	
 
 	/**
-	 * Constructeur de l'objet FicheChainee, il cree une nouvelle fiche chainee avec en parametre la premiere fiche
+	 * Constructeur de l'objet FicheChainee, il cree une nouvelle fiche chainee avec
+	 * en parametre la premiere fiche
+	 * 
 	 * @param f la fiche qui deviendra la premiere fiche
 	 */
-	public ListeFiche(FicheChaine f){
+	public ListeFiche(FicheChaine f) {
 		this.fiche = f;
 	}
-	
+
+	/**
+	 * Constructeur vide, créé une liste vide.
+	 */
+	public ListeFiche() {
+		this(null);
+	}
+
 	/**
 	 * Cette methode permet d'ajouter une fiche
+	 * 
 	 * @param f
 	 * @return
 	 */
-	public void ajouterFiche(FicheChaine f){
-		FicheChaine derniereFiche = this.getDerniereFiche();
-		derniereFiche.setFicheSuivante(f);
-		
+	public void ajouterFiche(Fiche f) {
+		if (this.isEmpty()) {
+			this.fiche = new FicheChaine(f);
+		} else {
+			FicheChaine derniereFiche = this.getDerniereFiche();
+			derniereFiche.setFicheSuivante(new FicheChaine(f));
+		}
 	}
-	
+
 	/**
 	 * Méthode permettant de retirer une fiche de la liste
+	 * 
 	 * @param index de la fiche à enlever
 	 */
-	public void retirerFiche(int index){
-		//si la fiche n'existe pas, la méthode s'arrête
-		if(index > 0 && index <= this.getSize()) {
-			FicheChaine supr = this.get(index);
-			FicheChaine avant = this.get(index-1);
+	public void retirerFiche(int index) {
+		// si la fiche n'existe pas, la méthode s'arrête
+		if (index > 0 && index <= this.getSize()) {
+			FicheChaine supr = this.getFicheChaine(index);
+			FicheChaine avant = this.getFicheChaine(index - 1);
 			avant.setFicheSuivante(supr.getFicheSuivante());
-			
-			//Suppression de la fiche
+
+			// Suppression de la fiche
 			supr = null;
 		}
 	}
-	
+
 	/**
 	 * Trie la liste en fonction du nom de famille
 	 */
-	public void trier(){
-		
+	public void trier() {
+
 	}
-	
+
 	/**
 	 * Méthode permettant de récuperer une des fiches
+	 * 
 	 * @param index index de la fiche choisie
 	 * @return La fiche numéro index
 	 */
-	public FicheChaine get(int index){
-		//Si l'index n'est pas dans la liste OU si la liste est vide
-		if(index <= 0 || index > this.getSize() || this.isEmpty()) {
+	public FicheChaine getFicheChaine(int index) {
+		// Si l'index n'est pas dans la liste OU si la liste est vide
+		if (index <= 0 || index > this.getSize() || this.isEmpty()) {
 			return null;
 		}
-		
+
 		int indexRech = 1;
-		
+
 		FicheChaine result = this.fiche;
-		
+
 		while (indexRech < index) {
 			result = result.getFicheSuivante();
 		}
-		
+
 		return result;
 	}
-	
-	/**
-	 * Méthode permettant de calculer la taille de la liste de fiches
-	 * @return le nombre de fiches contenues
-	 */
-	public int getSize(){
-		if(this.isEmpty()) {
-			return 0;
-		}
+
+	public Fiche getFiche(int index) {
+		final FicheChaine f = this.getFicheChaine(index);
 		
-		int size = 1;
-		
-		//Tant que la fiche suivante n'est pas la dernière fiche
-		while(!this.fiche.getFicheSuivante().estDerniereFiche()){
-			size++;
-		}
-		
-		
-		return size;
-	}
-	
-	public boolean isEmpty(){
-		return this.fiche != null;
-	}
-	
-	/**
-	 * Methode pour recuperer la derniere fiche
-	 * @return
-	 */
-	public FicheChaine getDerniereFiche() {
-		if(this.isEmpty()) {
+		if(f == null) {
 			return null;
 		}
 		
+		return (Fiche)f;
+	}
+
+	/**
+	 * Méthode permettant de calculer la taille de la liste de fiches
+	 * 
+	 * @return le nombre de fiches contenues
+	 */
+	public int getSize() {
+		if (this.isEmpty()) {
+			return 0;
+		}
+
+		int size = 1;
 		FicheChaine f = this.fiche;
-		
-		//Tant que f n'est pas la dernière fiche
+
+		// Tant que la fiche suivante n'est pas la dernière fiche
+		while (!f.estDerniereFiche()) {
+			f = f.getFicheSuivante();
+			size++;
+		}
+
+		return size;
+	}
+
+	public boolean isEmpty() {
+		return this.fiche == null;
+	}
+
+	/**
+	 * Methode pour recuperer la derniere fiche
+	 * 
+	 * @return
+	 */
+	public FicheChaine getDerniereFiche() {
+		if (this.isEmpty()) {
+			System.out.println(this.getSize());
+			return null;
+		}
+
+		FicheChaine f = this.fiche;
+
+		// Tant que f n'est pas la dernière fiche
 		while (!f.estDerniereFiche()) {
 			f = f.getFicheSuivante();
 		}
-		
+
 		return f;
 	}
 
-
-	
 }
