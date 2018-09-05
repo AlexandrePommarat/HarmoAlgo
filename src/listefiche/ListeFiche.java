97e60c1878c2b547501317004954e6eb2a5f7ac9
DimGael <dimgael98@gmail.com>
2018-09-05 11:09:43 +0200

    Ajout des méthodes retirer, getSize, isEmpty, et getDernièreFiche

--- src/listefiche/ListeFiche.java
+++ src/listefiche/ListeFiche.java
@@ -2,7 +2,7 @@ package listefiche;
 
 
 /**
- * Classe permettant de créer une liste de {@link FicheChaine} et de les manipuler
+ * Classe permettant de créer une liste de {@link FicheChaine} et de les manipuler, fiches indexées en commençant par 1.
  * @author gael 
  *
  */
@@ -27,10 +27,25 @@ public class ListeFiche {
 		return f;
 	}
 	
-	public void retirerFiche(){
-		
+	/**
+	 * Méthode permettant de retirer une fiche de la liste
+	 * @param index de la fiche à enlever
+	 */
+	public void retirerFiche(int index){
+		//si la fiche n'existe pas, la méthode s'arrête
+		if(index <= this.getSize()) {
+			FicheChaine supr = this.get(index);
+			FicheChaine avant = this.get(index-1);
+			avant.setFicheSuivante(supr.getFicheSuivante());
+			
+			//Suppression de la fiche
+			supr = null;
+		}
 	}
 	
+	/**
+	 * Trie la liste en fonction du nom de famille
+	 */
 	public void trier(){
 		
 	}
@@ -39,12 +54,28 @@ public class ListeFiche {
 		return null;
 	}
 	
+	/**
+	 * Méthode permettant de calculer la taille de la liste de fiches
+	 * @return le nombre de fiches contenues
+	 */
 	public int getSize(){
-		return 0;
+		if(this.isEmpty()) {
+			return 0;
+		}
+		
+		int size = 1;
+		
+		//Tant que la fiche suivante n'est pas la dernière fiche
+		while(!this.fiche.getFicheSuivante().estDerniereFiche()){
+			size++;
+		}
+		
+		
+		return size;
 	}
 	
 	public boolean isEmpty(){
-		return false;
+		return this.fiche != null;
 	}
 	
 	/**
@@ -52,8 +83,18 @@ public class ListeFiche {
 	 * @return
 	 */
 	public FicheChaine getDerniereFiche() {
-		return null;
+		if(this.isEmpty()) {
+			return null;
+		}
 		
+		FicheChaine f = this.fiche;
+		
+		//Tant que f n'est pas la dernière fiche
+		while (!f.estDerniereFiche()) {
+			f = f.getFicheSuivante();
+		}
+		
+		return f;
 	}
 
 
