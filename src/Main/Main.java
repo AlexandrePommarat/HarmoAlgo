327f7cd9014625402e9c72428654847bc1b0c4e5
Pommarat Alexandre <alexandre.pommarat@etu.unilim.fr>
2018-09-05 10:34:08 +0200

    Création d'une fiche chainée

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -11,10 +11,12 @@ public class Main {
 		while(!quitter){
 			System.out.println("Sélectionnez une action à effectuer:");
 			System.out.println("A. Afficher fiches");
-			
 			System.out.println("B. Afficher une fiche");
-			System.out.println("2. Ajouter une fiche");
-			System.out.println("3. ");
+			System.out.println("C. Ajouter une fiche");
+			System.out.println("D. Supprimer une fiche");
+			System.out.println("E. Rechercher une fiche");
+			System.out.println("F. Trier le tableau");
+			System.out.println("G. Quitter");
 		}
 		
 	}
